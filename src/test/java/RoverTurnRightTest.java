8699de1d0617bfb2096c2823e2cca04e1f131da8
SonlyF5020 <502089948@163.com>
2013-07-30 13:36:11 +0800

    Test  shouldFaceEastWhenTurnRightFromNorth

--- src/test/java/RoverTurnRightTest.java
+++ src/test/java/RoverTurnRightTest.java
@@ -1,4 +1,17 @@
+import com.zhanhonglai.Rover;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 
 public class RoverTurnRightTest {
-
+    @Test
+    public void shouldFaceEastWhenTurnRightFromNorth() throws Exception {
+        //GIVEN
+        Rover rover = new Rover();
+        rover.setDirection("NORTH");
+        //WHEN
+        rover.turnRight();
+        //THEN
+        assertThat(rover.getDirection(),is("EAST"));
+    }
 }
