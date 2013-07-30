9fe75baf66efd2e00ec9d5b5db9bc3b18b313fd3
SonlyF5020 <502089948@163.com>
2013-07-30 21:29:31 +0800

    move west test

--- src/test/java/RoverMoveAheadTest.java
+++ src/test/java/RoverMoveAheadTest.java
@@ -18,4 +18,18 @@ public class RoverMoveAheadTest {
         assertThat(rover.getXPosition(),is(3));
         assertThat(rover.getYPosition(),is(2));
     }
+
+    @Test
+    public void shouldArrive_1_2_whenMoveEastFrom_2_2() throws Exception {
+        //GIVEN
+        Rover rover = new Rover();
+        rover.setDirection("WEST");
+        rover.setPosition(2,2);
+        //WHEN
+        rover.move();
+        //THEN
+        assertThat(rover.getDirection(),is("WEST"));
+        assertThat(rover.getXPosition(),is(1));
+        assertThat(rover.getYPosition(),is(2));
+    }
 }
