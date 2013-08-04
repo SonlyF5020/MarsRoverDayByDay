eb98a22fadfeee944c166d37ba3e4bd7fe149527
SonlyF5020 <502089948@163.com>
2013-08-05 07:38:50 +0800

    add rover turn right test

---
+++ src/test/java/RoverTurnRightTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 8/5/13
+ * Time: 7:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoverTurnRightTest {
+}
