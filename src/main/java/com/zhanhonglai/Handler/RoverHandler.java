231efcb1e824ea484bceb0351ac22efe57388ad0
SonlyF5020 <502089948@163.com>
2013-08-05 13:11:00 +0800

    add handler test

---
+++ src/main/java/com/zhanhonglai/Handler/RoverHandler.java
@@ -0,0 +1,19 @@
+package com.zhanhonglai.handler;
+
+import com.zhanhonglai.Rover;
+
+public class RoverHandler {
+    private Rover rover;
+
+    public void setRover(Rover rover) {
+        this.rover = rover;
+    }
+
+    public Rover getRover() {
+        return rover;
+    }
+
+    public void initialPosition(String positionString) {
+
+    }
+}
--- src/test/java/functionTest/InitializeTest.java
+++ src/test/java/functionTest/InitializeTest.java
@@ -1,4 +1,24 @@
 package functionTest;
 
+import com.zhanhonglai.handler.RoverHandler;
+import com.zhanhonglai.Rover;
+import com.zhanhonglai.position.RoverPosition;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class InitializeTest {
+    @Test
+    public void shouldInitialPositionWhenInputString2_3() throws Exception {
+        //GIVEN
+        Rover rover = new Rover();
+        RoverHandler roverHandler = new RoverHandler();
+        roverHandler.setRover(rover);
+        RoverPosition expectedPosition = new RoverPosition(2,3);
+        //WHEN
+        roverHandler.initialPosition("2 3");
+        //THEN
+        assertThat(rover.getPosition(),equalTo(expectedPosition));
+    }
 }
